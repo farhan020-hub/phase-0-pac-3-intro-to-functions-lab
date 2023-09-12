b34a79a97b0e2fc44c2951ee97123e6ca12f19a6
AbdiazizSaman <farhanadam394@gmail.com>
2023-09-12 18:47:56 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,41 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+
+  function logShout(string) {
+
+    const uppercaseString = string.toUpperCase();
+
+    console.log(uppercaseString);
+  }
+
+  logShout('warrup');
+
+  function logWhisper(string) {
+
+    const lowercaseString = string.toLowerCase();
+
+    console.log(lowercaseString);
+  }
+
+  logWhisper('wagwan');
+
+
+  function sayHiToHeadphonedRoommate(string) {
+
+    if (string === string.toLowerCase()){
+        return "I can't hear you!";
+
+    } else if (string === string.toUpperCase()){
+        return 'YES INDEED!';
+
+    } else if (string === "Let's have dinner together!"){
+        return "I would love to!";
+    }
+
+
+  }
